df732118def93945cf045e9c1e1811a723ceb981
prudence107 <prudencegachoki290@gmail.com>
2024-07-29 19:40:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
+let myString = "Hello, World!";
+let myNumber = 42;
+let myBoolean = true;
+let myNull = null;
+let myUndefined;
+let myObject = { name: "Alice", age: 30 };
+let mySymbol = Symbol("unique");
 
+console.log(myString);
+console.log(myNumber);
+console.log(myBoolean);
+console.log(myNull);
+console.log(myUndefined);
+console.log(myObject);
+console.log(mySymbol);
+
+
+let numbers = [1, 2, 3, 4, 5];
+
+let doubledNumbers = numbers.map(num => num * 2);
+
+let filteredNumbers = numbers.filter(num => num > 10);
+
+let sum = numbers.reduce((acc, num) => acc + num, 0);
+
+numbers.forEach(num => console.log(num));
+
+
+
+function greet(name) {
+    console.log(Hello, ${name}!);
+}
+
+function sumArray(arr) {
+    return arr.reduce((acc, num) => acc + num, 0);
+}
+
+function findMax(arr) {
+    return Math.max(...arr);
+}
